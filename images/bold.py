92fbf51d439e7cc34dd2db39a8c21d68febbfecd
Freddy Cubas <Hexagonalstar64@gmail.com>
2026-09-23 22:35:51 -0400

    Format Python

--- images/bold.py
+++ images/bold.py
@@ -11,20 +11,20 @@ from sys import argv
 import xml.etree.ElementTree as ET
 
 if len(argv) != 3:
-    print('python bold.py <input.svg> <output.svg>')
+    print("python bold.py <input.svg> <output.svg>")
 
-ET.register_namespace('', "http://www.w3.org/2000/svg")
+ET.register_namespace("", "http://www.w3.org/2000/svg")
 
 tree = ET.parse(argv[1])
 svg = tree.getroot()
-assert(svg.tag == '{http://www.w3.org/2000/svg}svg')
+assert svg.tag == "{http://www.w3.org/2000/svg}svg"
 g = svg[0]
-assert(g.tag == '{http://www.w3.org/2000/svg}g')
+assert g.tag == "{http://www.w3.org/2000/svg}g"
 
 for child in g:
-    if 'class' in child.attrib and child.attrib['class'] == 'node':
+    if "class" in child.attrib and child.attrib["class"] == "node":
         text = child[2]
-        assert(text.tag == '{http://www.w3.org/2000/svg}text')
-        text.attrib['font-weight'] = 'bold'
+        assert text.tag == "{http://www.w3.org/2000/svg}text"
+        text.attrib["font-weight"] = "bold"
 
 tree.write(argv[2])
